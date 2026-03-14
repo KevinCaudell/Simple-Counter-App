bed3415b11d1c25306c2110238467ce8abee396a
Kevin Caudell <kevin.caudell.1@gmail.com>
2026-03-14 18:55:54 -0400

    Added local storage to save counter data

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let counter = 0;
+if (!localStorage){
+    localStorage.setItem('count', 0);
+};
+
+let counter = localStorage.getItem('count');
+console.log(localStorage.getItem('count'));
+
 
 const count = document.getElementById('count');
 const increase_btn = document.getElementById('increase-btn');
@@ -7,15 +13,18 @@ const reset_btn = document.getElementById('reset-btn');
 
 increase_btn.addEventListener('click', ()=>{
     counter ++;
+    localStorage.setItem('count', counter);
     count.textContent = counter;
 });
 
 decrease_btn.addEventListener('click', ()=>{
     counter --;
+    localStorage.setItem('count', counter);
     count.textContent = counter;
 });
 
 reset_btn.addEventListener('click', ()=>{
     counter = 0;
+    localStorage.setItem('count', counter);
     count.textContent = counter;
 });
